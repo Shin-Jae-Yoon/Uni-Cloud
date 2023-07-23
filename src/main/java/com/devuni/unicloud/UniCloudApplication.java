74353b9cba6f242e4b9a7e0e720010b3a61077b4
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-07-23 22:01:10 +0900

    chore: 프로젝트 초기 설정

--- src/main/java/com/devuni/unicloud/UniCloudApplication.java
+++ src/main/java/com/devuni/unicloud/UniCloudApplication.java
@@ -2,11 +2,14 @@ package com.devuni.unicloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class UniCloudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UniCloudApplication.class, args);
     }
+
 }
